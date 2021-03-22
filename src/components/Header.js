b3aa472a08ce5b0fd813bac626ff2ed63cf705ba
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-03-22 17:44:39 +0100

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,28 +5,41 @@ import Mic from '../assets/img/sch.png'
 import SearchIcon from '../assets/img/search.png'
 
 
- const Header = () => {
+ const div = () => {
    
     return (
        <>
-            <header className="headerWrapper">
-                <div className="logo">
-                    <img src={Logo} />
-                </div>
-                <div className="search-container">
-                    <div className="inputWrapper">
-                        <div className="search-input">
-                            <input type="text" placeholder="search posts..." />
-                         </div>
-                        <div className="children-vertical-align p-3 seach-icons">
-                            <img  src={Mic} width="25" className="icon-margin" />
-                            <img  src={SearchIcon} width="15" className="icon-margin" />
-                            
+            <header className="header-container">
+                <div className="headerWrapper">
+                    <div className="logo">
+                        <img src={Logo} />
+                    </div>
+                    <div className="search-container">
+                        <div className="inputWrapper">
+                            <div className="search-input">
+                                <input type="text" placeholder="search posts..."  className="w-100" />
+                            </div>
+                            <div className="children-vertical-align p-3 seach-icons">
+                                <img  src={Mic} width="25" className="icon-margin" />
+                                <img  src={SearchIcon} width="15" className="icon-margin" />
+                            </div>
                         </div>
+                    </div>
+                </div>
+                <div className="nav-menu-wrapper">
+                    <div className="empty-wrapper"></div>
+                    <div className="nav-menu">
+                        <ul>
+                            <li className="active">All</li>
+                            <li>Web</li>
+                            <li>Iamges</li>
+                            <li>Social</li>
+                            <li>Documents</li>
+                        </ul>
                     </div>
                 </div>
             </header>
        </>
     )
 }
-export default withRouter(Header)
+export default withRouter(div)
