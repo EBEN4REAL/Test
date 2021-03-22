670375ef3fed117cfbea0f48009075a5c67fc886
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-03-22 16:22:40 +0100

    .

--- src/components/createNews.js
+++ src/components/createNews.js
@@ -1,24 +1,46 @@
 import React from 'react'
+import {useState} from 'react'
 
-
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
 
 const CreateNews = ()  => {
+    const [body, setBody] = useState('')
+    const [title, setTitle] = useState('')
+
+    const  generateRandomNumber = () => {
+        return Math.floor(Math.random() * 9)
+    }
+
+    const createPost = (e) => {
+        e.preventDefault();
+        if(title === '') {
+            alert("TItle is required")
+            return
+        }
+        if(body === '') {
+            alert("Body is required")
+            return
+        }
+        const data = {
+            title: 'foo',
+            body: 'bar',
+            userId: generateRandomNumber()
+        }
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
     return (
         <div>
             <form>
+                <input type="text" placeholder="Title..." onChange={(e) => setTitle(e.target.value)} />
+                <input type="text" placeholder="Body..." onChange={(e) => setBody(e.target.value)}  />
+                <button onClick={(e) => createPost(e)}>Create</button>
             </form>
             
         </div>
